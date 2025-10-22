a6b9d163dab9a4bad272f827d8dd4e2a55dd4352
ronaldz012 <rzepita02@gmail.com>
2025-10-22 19:15:03 -0400

    Update: Remove Old User Repo

--- src/modules/auth/Auth.Data/DataDependencyInjection.cs
+++ src/modules/auth/Auth.Data/DataDependencyInjection.cs
@@ -1,16 +1,11 @@
-using System;
-
+using Auth.Data.Persistence;
+using Microsoft.Extensions.DependencyInjection;
 namespace Auth.Data;
 
-using System;
-using Auth.Data.Persistence;
-using Auth.Data.Repositories;
-using Microsoft.Extensions.DependencyInjection;
 
 public static class DataDependencyInjection
 {
     public static IServiceCollection AddAuthData(this IServiceCollection services)
-        => services.AddScoped<IUserRepository, UserRepository>()
-                   .AddScoped<AuthDbContext>();
+        => services.AddScoped<AuthDbContext>();
               
 }
